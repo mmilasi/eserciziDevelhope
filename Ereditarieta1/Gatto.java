011aa6e51ab378ffda03f2746b5970a9ba258871
Marija Milasinovic <30419423+mmilasi@users.noreply.github.com>
2024-01-12 15:57:44 +0100

    Update Gatto.java

--- Ereditarieta1/Gatto.java
+++ Ereditarieta1/Gatto.java
@@ -2,6 +2,6 @@ package Ereditarieta1;
 
 public class Gatto extends Animale{
     public static void faIlVerso() {
-        System.out.println("Non sono il tuo psicoterapeuta");
+        System.out.println("Il gatto fa: 'Non sono il tuo psicoterapeuta'");
     }
 }
