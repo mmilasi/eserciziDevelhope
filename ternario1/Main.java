4f51146a7e7096ee0d7fad1db65ee871ada2125a
Marija Milasinovic <30419423+mmilasi@users.noreply.github.com>
2024-02-13 15:46:02 +0100

    add ternario1

---
+++ ternario1/Main.java
@@ -0,0 +1,17 @@
+package ternario1;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println("Valore: " + i);
+            System.out.println(checkValue(i));
+        }
+    }
+
+    private static String checkValue(int value) {
+        String result = value % 5 == 0 ? "FizzBuzz" : "Buzz";
+
+        return result;
+    }
+
+}
